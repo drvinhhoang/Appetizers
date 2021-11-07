ad802544edf6dd1e75830069354e667289369d72
vinh <dr.vinhhoang@gmail.com>
2021-11-07 14:25:50 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by VINH HOANG on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
